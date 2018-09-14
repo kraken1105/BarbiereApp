8dcc9aa6a1f6181edba6a8d711708ff705dcda43
rznanni <giovanniradunanza@gmail.com>
2018-09-14 15:55:19 +0200

    jgvhcvh

--- app/src/main/java/com/picradrof/barbiereapp/userClient/LoginActivity.java
+++ app/src/main/java/com/picradrof/barbiereapp/userClient/LoginActivity.java
@@ -30,6 +30,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.picradrof.barbiereapp.R;
+import com.picradrof.barbiereapp.utility.gestionedb.DBHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,6 +69,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        DBHandler db = new DBHandler(this);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
